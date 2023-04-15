31f906983cc7e62529246268ae800eb9d3ee94c0
imdivyachoudhary <divyachoudhary.avnc@gmail.com>
2023-04-16 00:50:08 +0530

    MongoDb Connection

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const db = require("./config/mongoose");
 
 app.use("/", require("./routes"));
 
-app.set("view engine","ejs");
-app.set("views","./views");
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static("./assets"));
 
 app.listen(port, function (err) {
   if (err) {
